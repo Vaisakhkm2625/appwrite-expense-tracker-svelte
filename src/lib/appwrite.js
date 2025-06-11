bd0e89ab9b98b927e7482838ca565be076986044
Vaisakh K M <vaisakhkm2625@gmail.com>
2025-06-11 19:38:44 +0530

    appwritejs

---
+++ src/lib/appwrite.js
@@ -0,0 +1,17 @@
+import { Client , Account } from 'appwrite';
+import { 
+    PUBLIC_APPWRITE_ENDPOINT,
+    PUBLIC_APPWRITE_PROJECT_ID, 
+    PUBLIC_APPWRITE_COLLECTION_ID,
+    PUBLIC_APPWRITE_DATABASE_ID
+} from '$env/static/public';
+
+export const client = new Client();
+
+client
+    .setEndpoint(PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(PUBLIC_APPWRITE_PROJECT_ID);
+
+export const account = new Account(client);
+
+export {ID} from 'appwrite';
