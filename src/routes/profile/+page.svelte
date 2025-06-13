7cc17cef004d8a2a267650dfd10a3c2781db065f
Vaisakh K M <vaisakhkm2625@gmail.com>
2025-06-13 08:40:15 +0530

    added profile

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+
+import {currentUser} from "$lib/stores/auth.svelte";
+
+let keys = Object.keys(currentUser.user)
+console.log(keys);
+</script>
+
+<!-- {JSON.stringify(currentUser.user)} -->
+
+<form action="">
+    {#if currentUser.user}
+        {#each keys as key }
+            <label for={key}>{key.toUpperCase()}<input id={key} value={currentUser.user[key]} disabled></label>
+        {/each}
+    {/if}
+</form>
